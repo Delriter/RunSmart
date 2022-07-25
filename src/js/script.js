6e5e05a3d7187bead1d277cb782a4a8807f2afd2
AV <anton.vasichkin@hotmail.com>
2022-07-25 15:04:30 +0300

    commit?

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png" alt="arrow"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                dots: true,
+            }
+        }]
+    });
+});
