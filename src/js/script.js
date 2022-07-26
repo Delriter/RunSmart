519c7ef07b740b15f85033ee9775003dc48bba36
AV <anton.vasichkin@hotmail.com>
2022-07-26 21:44:31 +0300

    tiny slider added

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1200,
         autoplay: true,
         autoplaySpeed: 1500,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png" alt="arrow"></button>',
         responsive: [{
@@ -14,4 +15,21 @@ $(document).ready(function(){
             }
         }]
     });
+}); */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    navPosition: "bottom",
+    nav: false,
 });
+
+document.querySelector('.prev').addEventListener('click',  function () {
+    slider.goTo('prev');
+  });
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
